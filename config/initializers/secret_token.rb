96a2098eff2dd16796ea95347699f14f4303154b
Ward Penney <ward.penney@gmail.com>
2013-07-16 18:25:32 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsPrototypeStack::Application.config.secret_token = '8e8601615c6b15ecce24e381be9a72848d2674a34b8b89a04b7de0c7b8d10c546494a486c6cf9dea772f44fceb59d115ee0bfa63b2a1070d0bc8818402f0d539'
---
+++ readme.md
@@ -0,0 +1,7 @@
+# Rails Prototyping Stack
+A starter project for rails prototyping. 
+* SASS, haml
+* d3.v3
+* jQuery 1.9.1
+* modernizr 2.6.2 custom (css_pointerevents)
+* Bootstrap 2.2.1
